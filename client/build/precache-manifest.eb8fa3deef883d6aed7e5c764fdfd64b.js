6003cb3d8e245597e9cab51a6eae99b9f7880e52
Vikas Sheoran <kkviks@gmail.com>
2022-03-05 15:08:29 +0530

    Frontend: Add basic-blog react app

---
+++ client/build/precache-manifest.eb8fa3deef883d6aed7e5c764fdfd64b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4686d89d7ae2553fa1f41b84ef771fc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf0193a8fc2a0016205a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "340aff43ea85d70bab1f",
+    "url": "/static/css/main.b5fec8d3.chunk.css"
+  },
+  {
+    "revision": "cf0193a8fc2a0016205a",
+    "url": "/static/js/2.8091b331.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.8091b331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "340aff43ea85d70bab1f",
+    "url": "/static/js/main.ff4da0b1.chunk.js"
+  },
+  {
+    "revision": "07858fea14fd66493418",
+    "url": "/static/js/runtime-main.efee8004.js"
+  }
+]);
